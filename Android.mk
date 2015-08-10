1b9af67d5e0fa2ffa936716a53549432f415de8e
B--B <dark.schneider.ds@gmail.com>
2015-08-11 00:32:27 +0200

    Fix the build after latest changes

--- Android.mk
+++ Android.mk
@@ -18,9 +18,11 @@ LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 LOCAL_PACKAGE_NAME := Gallery2
 
+LOCAL_CERTIFICATE := platform
+
 LOCAL_OVERRIDES_PACKAGES := Gallery Gallery3D GalleryNew3D
 
-LOCAL_SDK_VERSION := current
+#LOCAL_SDK_VERSION := current
 
 LOCAL_JNI_SHARED_LIBRARIES := libjni_eglfence libjni_filtershow_filters librsjni libjni_jpegstream
 
